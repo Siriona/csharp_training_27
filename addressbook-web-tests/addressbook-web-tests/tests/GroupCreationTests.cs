b2a5ac8fe94656138daf09a1c9091f2bdc0b9041
Siriona <siriona7@gmail.com>
2022-01-30 18:53:33 +0300

    Text generators for groups

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -4,6 +4,8 @@ using OpenQA.Selenium.Firefox;
 using System;
 using System.Text;
 using System.Collections.Generic;
+using System.IO;
+
 
 
 
@@ -33,7 +35,45 @@ namespace WebAddressbookTests
         }
 
         
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
 
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                }
+                );
+            }
+            
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
+        public void GroupCreationTest(GroupData group)
+        {
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            Assert.AreEqual(oldGroups.Count + 1, app.Groups.GetGroupCount());
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+            // Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);  
+
+        }
+
+        /*
         [Test, TestCaseSource("RandomGroupDataProvider")]
         public void GroupCreationTest(GroupData group)
         {
@@ -50,14 +90,15 @@ namespace WebAddressbookTests
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
             // Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);  
-        }
-         
-
-       
-
-
-    
         
+        }
+        */
+
+
+
+
+
+
         [Test]
         public void BadNameGroupCreationTest()
         {
