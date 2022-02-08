1d4b5354382d12c0d31b38549ce801050b9ac876
Siriona <siriona7@gmail.com>
2022-02-08 20:33:14 +0300

    Task 18 new 2

--- addressbook_tests_white/addressbook_tests_white/test/GroupRemovingTests.cs
+++ addressbook_tests_white/addressbook_tests_white/test/GroupRemovingTests.cs
@@ -12,30 +12,34 @@ namespace addressbook_tests_white
         {
 
 
-            
+
             GroupData newGroup = new GroupData()
             {
                 Name = "01white"
             };
-            
-
-            // to be sure at least one group exists and to keep test data existing
-            // if disable -> the first group in list will be removed if it exists 
-            app.Groups.Add(newGroup);
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
-            GroupData groupToRemove = app.Groups.GetGroupForRemove();
 
-            app.Groups.Remove();
+            if (oldGroups.Count == 1) //application doesn't allow to delete the last group, at least one group always exists
+            {
+                app.Groups.Add(newGroup);
+            }
 
-            
+            GroupData groupToRemove = app.Groups.GetGroupList()[0];
+
+
+            app.Groups.Remove(groupToRemove);
+
+
             List<GroupData> newGroups = app.Groups.GetGroupList();
-            oldGroups.Remove(groupToRemove); 
+            oldGroups.Remove(groupToRemove);
             oldGroups.Sort();
             newGroups.Sort();
-            
+
             Assert.AreEqual(oldGroups, newGroups);
-            
+
+
+
 
         }
 
