0de9c16c7127cf237aaf51e75a83680a1146c96d
Siriona <siriona7@gmail.com>
2022-02-11 01:57:43 +0300

    Task 18 - 3

--- addressbook_tests_white/addressbook_tests_white/test/GroupRemovingTests.cs
+++ addressbook_tests_white/addressbook_tests_white/test/GroupRemovingTests.cs
@@ -15,7 +15,7 @@ namespace addressbook_tests_white
 
             GroupData newGroup = new GroupData()
             {
-                Name = "01white"
+                Name = "0000001-white"
             };
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
@@ -23,21 +23,33 @@ namespace addressbook_tests_white
             if (oldGroups.Count == 1) //application doesn't allow to delete the last group, at least one group always exists
             {
                 app.Groups.Add(newGroup);
+                oldGroups = app.Groups.GetGroupList();
+
             }
 
-            GroupData groupToRemove = app.Groups.GetGroupList()[0];
-
+            GroupData groupToRemove = oldGroups[0];
 
             app.Groups.Remove(groupToRemove);
 
 
             List<GroupData> newGroups = app.Groups.GetGroupList();
+
+
             oldGroups.Remove(groupToRemove);
             oldGroups.Sort();
             newGroups.Sort();
-
+          
             Assert.AreEqual(oldGroups, newGroups);
 
+            /*
+            foreach (GroupData old in oldGroups)
+                System.Console.WriteLine($"{old.Name}");
+
+            System.Console.WriteLine("----------------");
+
+            foreach (GroupData new1 in newGroups)
+                System.Console.WriteLine($"{new1.Name}");
+            */
 
 
 
